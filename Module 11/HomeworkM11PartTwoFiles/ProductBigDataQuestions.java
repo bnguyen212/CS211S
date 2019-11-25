76754da76872fbab09944d2c3b019f0c9b21db64
Brian Nguyen <bnguyen@rigetti.com>
2019-11-24 19:06:22 -0500

    completed m11 part 2

--- Module 11/HomeworkM11PartTwoFiles/ProductBigDataQuestions.java
+++ Module 11/HomeworkM11PartTwoFiles/ProductBigDataQuestions.java
@@ -8,49 +8,56 @@ public class ProductBigDataQuestions {
 
     public static void main(String[] args) {
     	List<Product> productList = createList();
- 
+
         int numberOfProducts = productList.size();
         System.out.println("Number of products \t\t\t\t\t73548: " + numberOfProducts);
-        
-        // QUESTION 1: How many total chemicals appear across all products? 
+
+        // QUESTION 1: How many total chemicals appear across all products?
         // Example: Product1 contains Chemical1, Chemical2 and Product2 contains Chemical2 and Chemical3
         //          count would be 4 total chemicals
-        int numberChemicalsInAllProducts = 0; 
+        int numberChemicalsInAllProducts = productList.stream().map(Product::getNumberOfChemicals).reduce(0, (c1, c2) -> c1 + c2);
         System.out.println("Number of chemicals in all products \t\t\t81790: " + numberChemicalsInAllProducts);
-     
+
         // QUESTION 2: How many different companies are in the dataset?
-        int numberCompanies = 0;
+        int numberCompanies = (int) productList.stream().map(Product::getCompany).distinct().count();
         System.out.println("Number of companies \t\t\t\t\t549: " + numberCompanies);
-        
+
         // QUESTION 3: How many products have 4 or more chemicals?
-        long numberProductsFourOrMore = 0;
-        System.out.println("Number of products with 4 or more chemicals \t\t193 :" + numberProductsFourOrMore);
+        long numberProductsFourOrMore = productList.stream().filter(p -> p.getNumberOfChemicals() >= 4).count();
+        System.out.println("Number of products with 4 or more chemicals \t\t193: " + numberProductsFourOrMore);
 
         //  QUESTION 4: Create a Map with key = company name and value = list of products for that company.
         // Hint: use Collectors.groupingBy!
-        Map<String, List<Product>> companyProductMap = null;
+        Map<String, List<Product>> companyProductMap = productList.stream().collect(Collectors.groupingBy(Product::getCompany));
         // checks that the map is correct; consider adding additional checks!
         System.out.println("Number of companies (keys) \t\t\t\t549: " + companyProductMap.size());
         System.out.println("Number of products for Aloette Cosmetics Inc. \t\t77: " + (companyProductMap.get("Aloette Cosmetics Inc.")).size());
         System.out.println("Number of products for Yves Rocher Inc. \t\t416: " + (companyProductMap.get("Yves Rocher Inc.")).size());
         System.out.println("Number of products for label.m USA INC \t\t\t4: " + (companyProductMap.get("label.m USA INC")).size());
-                
+
 
         // QUESTION 5: Use the map you created above. Which company has the most products?
         // Hint: Use .max(Comparator) and define the Comparator to compare companies based on size of their lists.
-        String companyMostProducts = "";
+        String companyMostProducts = companyProductMap.keySet().stream().max((c1, c2) -> Integer.compare(companyProductMap.get(c1).size(), companyProductMap.get(c2).size())).get();
         System.out.println("Company with most products \t\t\t\tLOreal USA: " + companyMostProducts);
-        
-       // Use this map for the next questions. 
+
+       // Use this map for the next questions.
        // This is a map with key = chemical name and value = list of products that contain that chemical.
        // This code creates a map with all chemicals and empty lists.
-       Map<String, List<Product>> chemicalProductMap = null;
-   
-       
+       Map<String, List<Product>> chemicalProductMap = new HashMap<String, List<Product>>();
+       productList.stream().forEach(
+               product -> {
+                   product.getChemicals().stream().
+                       forEach(chemicalName ->
+                           chemicalProductMap.putIfAbsent(chemicalName,  new ArrayList<Product>()));
+               }
+            );
+
+
        // QUESTION 6: Fill the lists (the value) of the map above.
        // Hint: Use a nested stream (one stream of productList and then a separate stream for each list of each product).
-       // YOUR CODE HERE- NO RETURN VALUE
-       
+        productList.stream().forEach(product -> product.getChemicals().stream().forEach(chemicalName -> chemicalProductMap.get(chemicalName).add(product)));
+
        // checks that the map is correct; consider adding additional checks!
        System.out.println("Number of products that contain Formaldehyde (gas) \t\t121: " + (chemicalProductMap.get("Formaldehyde (gas)")).size());
        System.out.println("Number of products that contain Lauramide DEA \t\t\t20: " + (chemicalProductMap.get("Lauramide DEA")).size());
@@ -58,7 +65,7 @@ public class ProductBigDataQuestions {
 
        // QUESTION 7: Which chemical appears in the most products?
        // Hint: use max(Comparator) again. Define your comparator to compare chemical names based on the size of the list of products.
-       String mostOccurringChemical = "";
+       String mostOccurringChemical = chemicalProductMap.keySet().stream().max((chem1, chem2) -> Integer.compare(chemicalProductMap.get(chem1).size(), chemicalProductMap.get(chem2).size())).get();
        System.out.println("Most common chemical \t\t\t\t\t\tTitanium dioxide: " + mostOccurringChemical);
        System.out.println("It appears in this many products \t\t\t\t67897: " + (chemicalProductMap.get("Titanium dioxide")).size());
 
@@ -69,10 +76,10 @@ public class ProductBigDataQuestions {
         String line = "";
         String fileName = "ChemicalData.csv";
         Map<Integer, Product> productMapByHash = new HashMap<Integer, Product>();
- 
+
         try (Scanner fileScan = new Scanner(
                 new FileReader(new File(fileName)))) {
-            line = fileScan.nextLine(); // column headers        
+            line = fileScan.nextLine(); // column headers
 
             while(fileScan.hasNext() ) {
                 line = fileScan.nextLine();
@@ -93,7 +100,7 @@ public class ProductBigDataQuestions {
                         category = categoryOption;
                     }
                 }
-               
+
                 Product newProduct = new Product(name, company, brand, colorScentFlavor, category);
 
                 int productHash = newProduct.hashCode();
@@ -104,7 +111,7 @@ public class ProductBigDataQuestions {
                    	newProduct.addChemical(chemicalName);
                    	productMapByHash.put(newProduct.hashCode(), newProduct);
                 }
-                
+
             }
         } catch (IOException ex){
             System.out.println(line);
